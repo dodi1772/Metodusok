5d5ade7a5d83295c5fb041642e86d80d40d1cd06
dodi1772 <zsigmond.dorian01@gmail.com>
2024-11-05 09:22:26 +0100

    Referenciaszintű csere

--- github/Program.cs
+++ github/Program.cs
@@ -4,11 +4,27 @@
 	{
 		static void Main(string[] args)
 		{
-			osszeadas();
-		}
-		static int osszeadas()
+			osszeadas(3,7);
+			Console.WriteLine(Duplaz(3));
+			int elso = 5;
+			int masodik = 10;
+			Csere(ref elso, ref masodik);
+            Console.WriteLine(elso+" "+masodik);
+        }
+		static int osszeadas(int a, int b)
 		{
-
+			return a + b;
+		}
+		static int Duplaz(int szam)
+		{
+			szam = szam * 2;
+			return szam;
+		}
+		static void Csere(ref int a,ref int b)
+		{
+			int temp = a;
+			a = b;
+			b = temp;
 		}
 	}
 }
